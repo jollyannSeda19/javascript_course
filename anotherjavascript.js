b03adb62444b7d8486c90cf544364313ff729f10
Jollyann Francez G. Seda <54280152+jollyannSeda19@users.noreply.github.com>
2026-02-28 17:54:57 +0000

    remove button

--- anotherjavascript.js
+++ anotherjavascript.js
@@ -1,11 +1,3 @@
-function showDate()  {
-    document.getElementById('demo').innerHTML=Date();
-}
-
-function hideDate() {
-    document.getElementById('demo').innerHTML="";
-}
-
 function mouseEnter() {
     let gameCanvas = document.getElementById('game');
     gameCanvas.addEventListener("mouseenter", function() {
